250faa455e4ad3d6971fa104670ce88247d3c2bf
Miguel Mamani <miguel.coder.pe@gmail.com>
2025-05-31 11:05:54 -0500

    Remove build dir

--- build/CMakeFiles/oreshnek_server.dir/src/net/EpollManager.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/oreshnek_server.dir/src/net/EpollManager.cpp.o: \
- /home/tecnodromo/lab/cpp/oreshnek/src/net/EpollManager.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/oreshnek_server.dir/src/net/SocketUtil.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/oreshnek_server.dir/src/net/SocketUtil.cpp.o: \
- /home/tecnodromo/lab/cpp/oreshnek/src/net/SocketUtil.cpp \
- /usr/include/stdc-predef.h
